6b087c4e9ef8f7c3b300d37e8b733c5365a11922
Clément Léger <clement.leger@bootlin.com>
2022-02-18 17:52:29 +0100

    i2c: of: use fwnode_get_i2c_adapter_by_node()
    Since the new fwnode based fwnode_find_i2c_adapter_by_node() function
    has the same behavior than of_get_i2c_adapter_by_node(), call it to
    avoid code duplication.
    
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- drivers/i2c/i2c-core-of.c
+++ drivers/i2c/i2c-core-of.c
@@ -113,17 +113,6 @@ void of_i2c_register_devices(struct i2c_adapter *adap)
 	of_node_put(bus);
 }
 
-static int of_dev_or_parent_node_match(struct device *dev, const void *data)
-{
-	if (dev->of_node == data)
-		return 1;
-
-	if (dev->parent)
-		return dev->parent->of_node == data;
-
-	return 0;
-}
-
 /* must call put_device() when done with returned i2c_client device */
 struct i2c_client *of_find_i2c_device_by_node(struct device_node *node)
 {
@@ -141,25 +130,6 @@ struct i2c_client *of_find_i2c_device_by_node(struct device_node *node)
 	return client;
 }
 EXPORT_SYMBOL(of_find_i2c_device_by_node);
-
-/* must call put_device() when done with returned i2c_adapter device */
-struct i2c_adapter *of_find_i2c_adapter_by_node(struct device_node *node)
-{
-	struct device *dev;
-	struct i2c_adapter *adapter;
-
-	dev = bus_find_device(&i2c_bus_type, NULL, node,
-			      of_dev_or_parent_node_match);
-	if (!dev)
-		return NULL;
-
-	adapter = i2c_verify_adapter(dev);
-	if (!adapter)
-		put_device(dev);
-
-	return adapter;
-}
-EXPORT_SYMBOL(of_find_i2c_adapter_by_node);
 
 /* must call i2c_put_adapter() when done with returned i2c_adapter device */
 struct i2c_adapter *of_get_i2c_adapter_by_node(struct device_node *node)
