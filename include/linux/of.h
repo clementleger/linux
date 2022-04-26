cc8d5ba96e974d7340564bdfc1b6ad39a9af20a2
Clément Léger <clement.leger@bootlin.com>
2022-04-26 17:04:22 +0200

    of: overlay: add of_overlay_fdt_apply_to_node()
    When using a driver which load an overlay and applies it on the of_node
    of device it represent, the target node isn't known in advance. This is
    for example the case when applying an overlay from a PCI driver. PCI
    cards might be plugged on whatever slot the user want and thus, the
    target node of the overlay framgents are unknown. This function allows
    to specify the node on which the overlay fragments will be applied.
    
    Signed-off-by: Clément Léger <clement.leger@bootlin.com>

--- include/linux/of.h
+++ include/linux/of.h
@@ -1590,8 +1590,8 @@ struct of_overlay_notify_data {
 
 #ifdef CONFIG_OF_OVERLAY
 
-int of_overlay_fdt_apply(const void *overlay_fdt, u32 overlay_fdt_size,
-			 int *ovcs_id);
+int of_overlay_fdt_apply_to_node(const void *overlay_fdt, u32 overlay_fdt_size,
+				 int *ret_ovcs_id, struct device_node *target);
 int of_overlay_remove(int *ovcs_id);
 int of_overlay_remove_all(void);
 
@@ -1600,8 +1600,9 @@ int of_overlay_notifier_unregister(struct notifier_block *nb);
 
 #else
 
-static inline int of_overlay_fdt_apply(void *overlay_fdt, u32 overlay_fdt_size,
-				       int *ovcs_id)
+static inline
+int of_overlay_fdt_apply_to_node(const void *overlay_fdt, u32 overlay_fdt_size,
+				 int *ret_ovcs_id, struct device_node *target)
 {
 	return -ENOTSUPP;
 }
@@ -1627,5 +1628,12 @@ static inline int of_overlay_notifier_unregister(struct notifier_block *nb)
 }
 
 #endif
+
+static inline int of_overlay_fdt_apply(const void *overlay_fdt,
+				       u32 overlay_fdt_size, int *ret_ovcs_id)
+{
+	return of_overlay_fdt_apply_to_node(overlay_fdt, overlay_fdt_size,
+					    ret_ovcs_id, NULL);
+}
 
 #endif /* _LINUX_OF_H */
